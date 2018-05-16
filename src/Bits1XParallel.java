86cfb5f4c8cecd760d81e544201bb07f4dd28acf
Rahim Rutledge <rahimrutledge@gmail.com>
2018-05-16 17:07:37 +0100

    New NFat files

---
+++ src/Bits1XParallel.java
@@ -0,0 +1,70 @@
+/** 
+ * A nondeterministic finite-state automaton that
+ * recognizes strings with 0 as the next-to-last
+ * character.
+ */
+public class Bits1XParallel{
+
+  /*
+   * The current set of states, encoded bitwise: 
+   * state i is represented by the bit 1<<i.
+   */
+  private int stateSet;
+
+
+  /**
+   * Reset the current state set to {the start state}.
+   */
+  public void reset() {
+    stateSet = 1<<0; // {q0}
+  }
+
+
+  /*
+   * The transition function represented as an array.
+   * The set of next states from a given state s and 
+   * character c is at delta[s,c-'0'].
+   */
+  static private int[][] delta = 
+     {{1<<0, 1<<0|1<<1}, // delta[q0,0] = {q0}
+                         // delta[q0,1] = {q0,q1}
+      {1<<2, 1<<2}, // delta[q1,0] = {q2}
+                    // delta[q1,1] = {q2}
+      {0, 0}}; // delta[q2,0] = {}
+               // delta[q2,1] = {}
+
+
+  /**
+   * Make one transition from state-set to state-set on 
+   * each char in the given string.
+   * @param in the String to use
+   */
+  public void process(String in) {
+    for (int i = 0; i < in.length(); i++) {
+      char c = in.charAt(i);
+      int nextSS = 0; // next state set, initially empty
+      for (int s = 0; s <= 2; s++) { // for each state s
+        if ((stateSet & (1<<s)) != 0) { // if maybe in s
+          try {
+            nextSS |= delta[s][c-'0'];
+          }
+          catch (ArrayIndexOutOfBoundsException ex) {
+            // in effect, nextSS |= 0
+          }
+        }
+      }
+      stateSet = nextSS; // new state set after c
+    }
+  }
+
+
+  /**
+   * Test whether the NFA accepted the string.
+   * @return true if the final set includes 
+   *         an accepting state.
+   */
+  public boolean accepted() {
+    return (stateSet & (1<<2))!=0; // true if q2 in set
+  }
+}
+
---
+++ src/Bits1XParallelFilter.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+/**
+ * A Java application to demonstrate the Bits0X class by
+ * using it to filter the standard input stream.  Those
+ * lines that are accepted by Bits0 are echoed to the
+ * standard output.
+ */
+public class Bits1XParallelFilter {
+  public static void main(String[] args) 
+        throws IOException {
+
+    Bits1XParallel m = new Bits1XParallel(); // the NFA
+    Scanner in =  // standard input
+      new Scanner(new File("bits.txt"));
+
+    // Read and echo lines until EOF.
+
+    String s = in.nextLine();
+    while (s!=null) {
+      m.reset();
+      m.process(s);
+      if (m.accepted()) System.out.println(s); // if bits accepted, otherwise skipped
+      else {System.out.println(s + " not accepted");}
+      s = in.next();
+    }
+  }
+}
