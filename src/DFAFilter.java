62e5c62302e08c9c58639a9eb5db576f1a5dff1d
MrRutledge <rahimrutledge@gmail.com>
2018-10-25 16:49:23 +0100

    changes: in the file eclipse file system

--- src/DFAFilter.java
+++ src/DFAFilter.java
@@ -17,22 +17,23 @@ public static void main(String[] args)
     DFA word = new DFA(); // The DFA
     
  // Get the word from the User
-    System.out.println("\t\nWhat word would you like to test? \n \t");
+    System.out.println(" What word would you like to test? \t");
     newWord = new Scanner(System.in);
 
     // Read and echo accepted words
     String w = newWord.nextLine();
-    while (w != null ) {
+    while (w != null
+    ) {
       word.reset();
       word.process(w);
       // If the Word gets to the final state
-       if (word.accepted()) System.out.println( w + " Word accepted");
+       if (word.accepted())  System.out.println( w + " Word accepted \nContinue or Press q to quite \t");
        // if the word fails to get to the final state
-         else{
-           System.out.println("Word rejected sorry");}
+         else{System.out.println( w + "Word rejected sorry \n Continue or Press q to quite");}
        //Read the next word 
             w = newWord.nextLine();
        }
-     }
-  }
+    
+    }
+}
 
